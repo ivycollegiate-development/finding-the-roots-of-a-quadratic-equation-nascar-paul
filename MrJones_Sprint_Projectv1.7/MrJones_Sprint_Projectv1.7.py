8706b6cafd8cffa1f8235b5d757344cb45fb8853
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-30 06:03:10 +0000

    Populates code to calculate the discriminate

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.7.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.7.py
@@ -1,1 +1,5 @@
-#Initial Commit
+#This is how we calculate the discriminate
+def calculate_discriminate(a, b, c):
+    return b**2 - 4ac
+#Returns the discriminate to the user in the terminal
+print(calculate_discriminate(1, -5, 6))
