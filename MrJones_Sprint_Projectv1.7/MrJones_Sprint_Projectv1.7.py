4eee37b7a0670b5dec275ccd5354047c0d048a39
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-30 06:06:31 +0000

    Fixes two syntax errors on line 3

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.7.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.7.py
@@ -1,5 +1,5 @@
 #This is how we calculate the discriminate
 def calculate_discriminate(a, b, c):
-    return b**2 - 4ac
+    return b**2 - 4*a*c
 #Returns the discriminate to the user in the terminal
 print(calculate_discriminate(1, -5, 6))
