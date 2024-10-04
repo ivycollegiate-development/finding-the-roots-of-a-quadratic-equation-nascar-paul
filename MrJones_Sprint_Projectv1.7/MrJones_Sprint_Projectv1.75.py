12e36c41cb0885f349f77e76a7b24e1d05689eba
Paul Jones <paul.tyler.jones@gmail.com>
2024-10-04 05:48:35 +0000

    Organizes comments

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
@@ -1,13 +1,15 @@
 # How to import a library into your script
 import cmath
 
-def calculate_complex_roots(a, b, c):
+def calculate_roots(a, b, c):
     # Returns the discriminate as an object to use in our if else statement
     discriminate = b**2 - 4*a*c
     # Here is where we will calculate the first root using
     # The cmath.sqrt function from the cmath library
-    root_1 = # this will need to be the plus half of the quadratic
-    root_2 = # this will need to be the minus half of the quadratic
+    # this will need to be the plus half of the quadratic
+    root_1 = 
+    # this will need to be the minus half of the quadratic
+    root_2 = 
     return root_1, root_2
 # Test your function
-print(calculate_complex_roots(1, 1, 1))  # Should print (3.0, 2.0)
+print(calculate_roots(1, 1, 1))  # Should print (3.0, 2.0)
