db210fcd5cfa872512e28c6af812251f7eaaa544
Paul Jones <paul.tyler.jones@gmail.com>
2024-10-04 05:59:00 +0000

    Changes line 6 to take the square root

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
@@ -3,13 +3,13 @@ import cmath
 
 def calculate_roots(a, b, c):
     # Returns the discriminate as an object to use in our if else statement
-    discriminate = b**2 - 4*a*c
+    discriminate = (b**2 - 4*a*c) ** 0.5
     # Here is where we will calculate the first root using
     # The cmath.sqrt function from the cmath library
     # this will need to be the plus half of the quadratic
-    root_1 = 
+    root_1 = (discriminate)
     # this will need to be the minus half of the quadratic
-    root_2 = 
+    root_2 = (discriminate)
     return root_1, root_2
 # Test your function
 print(calculate_roots(1, 1, 1))  # Should print (3.0, 2.0)
