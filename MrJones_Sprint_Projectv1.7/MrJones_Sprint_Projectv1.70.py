d491ca1cb786f130efebeae1e20b3e7fe108469f
Paul Jones <paul.tyler.jones@gmail.com>
2024-10-04 05:37:50 +0000

    Names fucntions to caclute quadratic roots

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.70.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.70.py
@@ -3,3 +3,4 @@ def calculate_discriminate(a, b, c):
     return b**2 - 4*a*c
 #Returns the discriminate to the user in the terminal
 print(calculate_discriminate(1, -5, 6))
+print(calculate_roots(1, 2, 1))   # Should print (-1.0, -1.0)
---
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
@@ -0,0 +1,13 @@
+# How to import a library into your script
+import cmath
+
+def calculate_complex_roots(a, b, c):
+    # Returns the discriminate as an object to use in our if else statement
+    discriminate = b**2 - 4*a*c
+    # Here is where we will calculate the first root using
+    # The cmath.sqrt function from the cmath library
+    root_1 = # this will need to be the plus half of the quadratic
+    root_2 = # this will need to be the minus half of the quadratic
+    return root_1, root_2
+# Test your function
+print(calculate_complex_roots(1, 1, 1))  # Should print (3.0, 2.0)
