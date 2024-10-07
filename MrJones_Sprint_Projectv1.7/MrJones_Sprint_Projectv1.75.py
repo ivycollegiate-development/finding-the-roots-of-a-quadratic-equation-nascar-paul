795ec9d870e5ab0fa3a45b1792920e5062aa1f1c
Paul Jones <paul.tyler.jones@gmail.com>
2024-10-07 05:52:04 +0000

    Adds code to calculte real roots

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
@@ -1,6 +1,5 @@
 # How to import a library into your script
 import math
-
 def calculate_roots(a, b, c):
     # Returns the discriminate as an object to use in our if else statement
     discriminate = (b**2 - 4*a*c) ** 0.5
@@ -12,5 +11,11 @@ def calculate_roots(a, b, c):
     root_2 = float((-b - discriminate) / (2*a))
     return root_1, root_2
 # Test your function
+if discriminate > 0:
+    print(f'There are two roots: {root_1} and {root_2}')
+elif discriminate == 0:
+    print(f'There is one real root at {root_1}')
+else:
+    return None
 
 print(calculate_roots(1, 1, 1))  # Should print (3.0, 2.0)
