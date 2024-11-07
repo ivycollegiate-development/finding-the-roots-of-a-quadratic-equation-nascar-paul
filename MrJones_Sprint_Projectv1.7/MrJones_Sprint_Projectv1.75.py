c49b4c145381e038daea48c3e8d7005e84d730c2
Paul Jones <paul.tyler.jones@gmail.com>
2024-11-07 05:13:22 +0000

    Ends class

--- MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
+++ MrJones_Sprint_Projectv1.7/MrJones_Sprint_Projectv1.75.py
@@ -1,21 +1,17 @@
 # How to import a library into your script
-import math
+import cmath
 def calculate_roots(a, b, c):
     # Returns the discriminate as an object to use in our if else statement
-    discriminate = (b**2 - 4*a*c) ** 0.5
-    # Here is where we will calculate the first root using
-    # The cmath.sqrt function from the cmath library
-    # this will need to be the plus half of the quadratic
-    root_1 = float((-b + discriminate) / (2*a))
+    discriminate = (b**2 - 4*a*c)
+    # Here is where we will calculate the first root using The cmath.sqrt function from the cmath library 
+    root_1 = (-b + cmath.sqrt(discriminate)) / (2*a)
     # this will need to be the minus half of the quadratic
-    root_2 = float((-b - discriminate) / (2*a))
-    return root_1, root_2
+    root_2 = (-b - cmath.sqrt(discriminate)) / (2*a)
 # Test your function
-if discriminate > 0:
-    print(f'There are two roots: {root_1} and {root_2}')
-elif discriminate == 0:
-    print(f'There is one real root at {root_1}')
-else:
-    return None
-
+    if discriminate > 0:
+        print(f'There are two roots: {root_1} and {root_2}')
+    elif discriminate == 0:
+        print(f'There is one real root at {root_1}')
+    else:
+        return root_1, root_2
 print(calculate_roots(1, 1, 1))  # Should print (3.0, 2.0)
